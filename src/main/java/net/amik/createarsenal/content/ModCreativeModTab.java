f7025b8a650e5b931daa6888b6da5d27934b6bce
cakeGit <65340665+cakeGit@users.noreply.github.com>
2023-03-26 12:24:45 +0100

    Ctab

--- src/main/java/net/amik/createarsenal/content/ModCreativeModTab.java
+++ src/main/java/net/amik/createarsenal/content/ModCreativeModTab.java
@@ -6,13 +6,13 @@ import net.minecraft.world.item.ItemStack;
 public class ModCreativeModTab{
 
 
-    public static final CreativeModeTab CREATE_ARSENAL_TAB_GENERAL = new CreativeModeTab("createarsenaltabgeneral") {
+    public static final CreativeModeTab CREATE_ARSENAL_TAB_GENERAL = new CreativeModeTab("createarsenal.tab.general") {
         @Override
         public ItemStack makeIcon() {return new ItemStack(ModItems.SPRING.get());
         }
     };
 
-    public static final CreativeModeTab CREATE_ARSENAL_TAB_BULLETS = new CreativeModeTab("createarsenaltabbullets") {
+    public static final CreativeModeTab CREATE_ARSENAL_TAB_BULLETS = new CreativeModeTab("createarsenal.tab.bullets") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(ModItems.BULLET_MEDIUM.get());
