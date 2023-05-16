52507fa8186f24611f3c927e95adf0373117328f
happygill <hsgill97@gmail.com>
2023-05-15 18:33:16 -0700

    4BarrelStatic Firing Logic(wip)

--- src/main/java/net/amik/createarsenal/block/staticTurret/FourBarrelTurret/FourBarrelStaticTurretTileEntity.java
+++ src/main/java/net/amik/createarsenal/block/staticTurret/FourBarrelTurret/FourBarrelStaticTurretTileEntity.java
@@ -2,17 +2,35 @@ package net.amik.createarsenal.block.staticTurret.FourBarrelTurret;
 
 import com.simibubi.create.content.contraptions.base.KineticTileEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import org.lwjgl.system.CallbackI;
+
+import static net.amik.createarsenal.block.staticTurret.FourBarrelTurret.FourBarrelStaticTurret.FACING;
 
 public class FourBarrelStaticTurretTileEntity extends KineticTileEntity {
+
     public FourBarrelStaticTurretTileEntity(BlockEntityType<?> typeIn, BlockPos pos, BlockState state) {
         super(typeIn, pos, state);
     }
 
     @Override
-    public void onSpeedChanged(float previousSpeed) {
-        super.onSpeedChanged(previousSpeed);
-        sendData();
+    public void lazyTick() {
+        if(targetInSight()&&getSpeed()>8)
+        {
+            System.out.println("Fire!");
+        }
+    }
+
+    private boolean targetInSight()
+    {
+        Direction dir= getBlockState().getValue(FACING);
+        AABB range= new AABB(getBlockPos().below(),getBlockPos().relative(dir,-16));
+        System.out.println(range);
+        System.out.println(level.getEntities(null,range));
+        return !level.getEntities(null,range).isEmpty();
     }
 }
