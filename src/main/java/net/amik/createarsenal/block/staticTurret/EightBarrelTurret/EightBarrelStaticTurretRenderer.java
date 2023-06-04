725ba15c5731f19ef9de98c9c1eef0567adacc28
Amiran <amikmain001@gmail.com>
2023-06-04 18:19:34 +0400

    Fixed turret block model having a static shaft Unfinished Eight barrel turret added

---
+++ src/main/java/net/amik/createarsenal/block/staticTurret/EightBarrelTurret/EightBarrelStaticTurretRenderer.java
@@ -0,0 +1,11 @@
+package net.amik.createarsenal.block.staticTurret.EightBarrelTurret;
+
+import com.simibubi.create.content.kinetics.base.KineticBlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+
+public class EightBarrelStaticTurretRenderer extends KineticBlockEntityRenderer {
+    public EightBarrelStaticTurretRenderer(BlockEntityRendererProvider.Context context) {
+        super(context);
+    }
+
+}
